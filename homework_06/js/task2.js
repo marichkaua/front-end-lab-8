d8a80742563466840ed30cd3d70d2d14b99c850b
Mariya Polyulyuk <34648412+marichkaua@users.noreply.github.com>
2018-02-10 21:10:10 +0200

    Update task2.js

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -5,5 +5,5 @@ var rD = 27.13;
 
 var exchangeE = e * rE;
 var exchangeD = d * rD;
-var exchangeED = rE/rD;
+var exchangeED = rE / rD;
 console.log(e + " Euros are equal " + exchangeE.toFixed(2) + " UAN, " + d + " Dollars are equal " + exchangeD.toFixed(2) + " UAN, one Euro is equal " + exchangeED.toFixed(2) + " Dollars.");
