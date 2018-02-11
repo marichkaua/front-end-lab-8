7fefd4861cca934aac288565cf3cab9847654421
Mariya Polyulyuk <34648412+marichkaua@users.noreply.github.com>
2018-02-11 21:18:37 +0200

    Code Validated

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -1,23 +1,22 @@
-var N = parseInt(prompt("Pls enter a natural number from 1 to 20"), 10);
-if (isNaN(N) || (N % 1 !== 0) || (N <= 0 && N > 20)) 
-{
-    console.error("Incorrect!")
-} else {
-    function pyramidStar(N) {
-        var str = "";
-        for (var i = 1; i <= N; i++) {
-            for (var j = 1; j <= N - i; j++) {
-                str += "   ";
-            }
-            for (var k = N - i + 1; k < N + i; k++) {
-                str += "[~]";
-            }
-            for (var m = N + i; m <= 2 * N - 1; m++) {
-                str += "   ";
-            }
-            str += "\n";
+var N, i, j, k, m;
+
+N = parseInt(prompt("Pls enter a natural number from 1 to 20"), 10);
+
+if (Number.isNaN(N) || (N % 1 !== 0) || (N <= 0 || N > 20)) {
+    console.error("Incorrect!");
+} else { 
+    var str = "";
+    for (i = 1; i <= N; i++) {
+        for (j = 1; j <= N - i; j++) {
+            str += "   ";
         }
-        return str;
+        for (k = N - i + 1; k < N + i; k++) {
+            str += "[~]";
+        }
+        for (m = N + i; m <= 2 * N - 1; m++) {
+            str += "   ";
+        }
+        str += "\n";
     }
-    console.log(pyramidStar(N))
+    console.log(str)
 }
