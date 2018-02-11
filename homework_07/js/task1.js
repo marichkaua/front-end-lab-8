ec0c9f6cbd73a79bae063b96a31697a244e7a7c0
Mariya Polyulyuk <34648412+marichkaua@users.noreply.github.com>
2018-02-11 18:23:27 +0200

    Update task1.js

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -1,1 +1,23 @@
-
+var N = parseInt(prompt("Pls enter a natural number from 1 to 20"), 10);
+if ((N != '') ||
+    (N(0 < N <= 20) || isNaN(N) === false) || (n % 1 === 0)) {
+    console.error("Incorrect!")
+} else {
+    function pyramidStar(N) {
+        var str = "";
+        for (var i = 1; i <= N; i++) {
+            for (var j = 1; j <= N - i; j++) {
+                str += "   ";
+            }
+            for (var k = N - i + 1; k < N + i; k++) {
+                str += "[~]";
+            }
+            for (var m = N + i; m <= 2 * N - 1; m++) {
+                str += "   ";
+            }
+            str += "\n";
+        }
+        return str;
+    }
+    console.log(pyramidStar(N))
+}
