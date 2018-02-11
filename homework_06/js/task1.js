abe119ecda029eaae147c1813db8b7ac9dcf3be2
Mariya Polyulyuk <34648412+marichkaua@users.noreply.github.com>
2018-02-11 15:57:29 +0200

    Added "Incorrect input" for the NaN & negative numbers

--- homework_06/js/task1.js
+++ homework_06/js/task1.js
@@ -2,12 +2,25 @@ var a = parseInt(prompt("Pls enter Side A"), 10);
 var b = parseInt(prompt("Pls enter Side B"), 10);
 var c = parseInt(prompt("Pls enter Side C"), 10);
 
-function getTriangleType(a, b, c) {
-    return ((((a * a) + (b * b)) === (c * c) || ((c * c) - (b * b)) === (a * a) || ((c * c) - (a * a)) === (b * b)) && 'right') || ((a === b && b === c) && 'equilateral') ||
-        ((a === b || a === c || b === c) && 'isosceles') || 'scalene';
+if ((a, b, c != '') ||
+    (isNaN(a, b, c) === false)) {
+    console.log("Incorrect data")
+} else {
+    function getTriangleType(a, b, c) {
+        return (((a * a) + (b * b)) === (c * c) ||
+                ((c * c) + (b * b)) === (a * a) ||
+                ((c * c) + (a * a)) === (b * b)) && 'right' ||
+            (a === b && b === c && c === a) && 'equilateral' ||
+            (a === b || a === c || b === c) && 'isosceles' ||
+            'scalene'
+    }
+    function AreaCalc(a, b, c) {
+        perimeter = (a + b + c) / 2;
+        areaVal1 = perimeter * ((perimeter - a) * (perimeter - b) * (perimeter - c));
+        if (areaVal1 < 0) {
+            areaVal1 = areaVal1 * (-1);
+        }
+        return Math.sqrt(areaVal1)
+    }
+    console.log("The type of a triangle is " + getTriangleType(a, b, c) + ". It's area: " + AreaCalc(a, b, c).toFixed(2) + ".");
 }
-
-var perimeter = (a + b + c) / 2;
-var area = Math.sqrt(perimeter * ((perimeter - a) * (perimeter - b) * (perimeter - c)));
-
-console.log("The type of a triangle is " + getTriangleType() + ". It's area: " + area.toFixed(2) + ".");
