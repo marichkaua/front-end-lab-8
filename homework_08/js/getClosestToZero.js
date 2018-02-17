e077315a62c3349fa011f0e8b3681180c003c2e2
Mariya Polyulyuk <34648412+marichkaua@users.noreply.github.com>
2018-02-17 22:19:03 +0200

    Update getClosestToZero.js

--- homework_08/js/getClosestToZero.js
+++ homework_08/js/getClosestToZero.js
@@ -1,1 +1,13 @@
+var mN;
 
+function getClosestToZero() {
+    mN = [];
+    for (var i = 0; i < arguments.length; i++) {
+        if (!isNaN(arguments[i])) {
+
+            mN.push(Math.abs(arguments[i]));
+        }
+    }
+
+    return Math.min.apply(-Math.abs(Math, mN));
+}
