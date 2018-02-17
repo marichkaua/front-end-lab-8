2d5f09132accc3c9b065dce1717d7174dedd0722
Mariya Polyulyuk <34648412+marichkaua@users.noreply.github.com>
2018-02-17 18:37:52 +0200

    Update getMin.js

--- homework_08/js/getMin.js
+++ homework_08/js/getMin.js
@@ -1,1 +1,12 @@
+var mN;
 
+function getMin() {
+    mN = [];
+    for (var i = 0; i < arguments.length; i++) {
+        if (!isNaN(arguments[i])) {
+            mN.push(arguments[i]);
+        }
+    }
+    return Math.min.apply(Math, mN);
+};
+console.log(getMin());
