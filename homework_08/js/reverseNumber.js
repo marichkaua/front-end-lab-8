9bce6a877c63547afeb5f439ac0d25f2df47a908
Mariya Polyulyuk <34648412+marichkaua@users.noreply.github.com>
2018-02-18 14:30:09 +0200

    Update reverseNumber.js

--- homework_08/js/reverseNumber.js
+++ homework_08/js/reverseNumber.js
@@ -1,6 +1,7 @@
 function reverseNum(n) {
-    if (n < 0) {return -reverseNum(-n);}
-    else {
-    return n.toString().split("").reverse().join("");
+    if (n < 0) {
+        return -reverseNum(-n);
+    } else {
+        return Number(n.toString().split("").reverse().join(""));
     }
 }
