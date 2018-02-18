53f147c7ead65729d3a34d386b98bb46579443c7
Mariya Polyulyuk <34648412+marichkaua@users.noreply.github.com>
2018-02-18 13:24:30 +0200

    Now negative numbers can be returned

--- homework_08/js/getClosestToZero.js
+++ homework_08/js/getClosestToZero.js
@@ -1,13 +1,25 @@
-var mN;
+var i, mN, k, nn;
 
 function getClosestToZero() {
     mN = [];
-    for (var i = 0; i < arguments.length; i++) {
-        if (!isNaN(arguments[i])) {
-
-            mN.push(Math.abs(arguments[i]));
+    for (i = 0; i < arguments.length; i++) {
+        if (!Number.isNaN(arguments[i])) {
+            mN.push(arguments[i]);
         }
     }
-
-    return Math.min.apply(Math, mN);
+    nn = Math.abs(mN[0]);
+    for (i = 0; i < mN.length - 1; i++) {
+        if (mN[i] < 0) {
+            if (nn > mN[i] * (-1)) {
+                nn = mN[i] * (-1);
+                k = i;
+            }
+        } else {
+            if (nn > mN[i]) {
+                nn = mN[i];
+                k = i;
+            }
+        }
+    }
+    return mN[k];
 }
