e38694113a8bcceb9df239a9a18d632a83556e41
Mariya Polyulyuk <34648412+marichkaua@users.noreply.github.com>
2018-02-19 16:15:16 +0200

    Update getTransformedArray.js

--- homework_09/js/getTransformedArray.js
+++ homework_09/js/getTransformedArray.js
@@ -1,1 +1,10 @@
+var i, k;
 
+function newArray(array, someF) {
+    var newNewArray = [];
+    for (i = 0; i < array.length; i++) {
+        k = someF(array[i]);
+        newNewArray.push(k);
+    }
+    return newNewArray;
+}
