295dee87a838544fe0c0a58c89d6b4d16659dc7d
Mariya Polyulyuk <34648412+marichkaua@users.noreply.github.com>
2018-02-20 20:29:28 +0200

    Update forEach.js

--- homework_09/js/forEach.js
+++ homework_09/js/forEach.js
@@ -1,7 +1,7 @@
-function forEach (array, callfunc) { 
-    newArray = [];
-    for (i = 0; i < array.length; i ++) {
-    newArray.push(callfunc(array[i]));
+function forEach(arrayA, someF) {
+    for (var i = 0; i < arrayA.length; i++) {
+        if (typeof someF == "function") {
+            someF(arrayA[i]);
+        }
     }
-    return newArray;
 }
