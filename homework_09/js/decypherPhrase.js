0833dba3ee7fd3d29c58740ed75c8b5dbf331939
Mariya Polyulyuk <34648412+marichkaua@users.noreply.github.com>
2018-02-20 22:00:16 +0200

    Update decypherPhrase.js

--- homework_09/js/decypherPhrase.js
+++ homework_09/js/decypherPhrase.js
@@ -1,1 +1,9 @@
+function decypherPhrase(rulesMap, inputString) {
+    var swappedMap = {};
 
+    for (var key in rulesMap) {
+        swappedMap[rulesMap[key]] = key;
+    }
+
+    return cypherPhrase(swappedMap, inputString);
+}
